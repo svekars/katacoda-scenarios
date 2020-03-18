674560b0eb34f02edafd70ddc865bf53130d98e1
Svetlana Karslioglu <svetlana.karslioglu@gmail.com>
2020-03-18 09:51:03 -0700

    Update

--- getting-started/assets/launch.sh
+++ getting-started/assets/launch.sh
@@ -1,8 +1,8 @@
-#echo "Starting Kubernetes..."
-##minikube start
-#while [ `docker ps | wc -l` -eq 1 ]
-#do
-#  sleep 1
-#done
+echo "Starting Kubernetes..."
+minikube start
+while [ `docker ps | wc -l` -eq 1 ]
+do
+  sleep 1
+done
 
-#echo "Kubernetes Started"
+echo "Kubernetes Started"
