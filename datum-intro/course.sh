545aa958edbd4d53db045e33807aec321b705904
svekars <svetlana.karslioglu@gmail.com>
2020-04-03 15:19:58 -0700

    Update

--- datum-intro/course.sh
+++ datum-intro/course.sh
@@ -1,3 +1,3 @@
-ssh root@[[HOST1_IP]] "curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb && sudo dpkg -i /tmp/pachctl.deb && sleep 10 && pachctl deploy local"
+ssh root@[[HOST1_IP]] "curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb && sudo dpkg -i /tmp/pachctl.deb; do sleep 5; done; pachctl deploy local"
 
 
