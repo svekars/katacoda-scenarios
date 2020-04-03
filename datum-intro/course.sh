dacf4b8609aed4502643a712e7311a0f6f8f5e77
svekars <svetlana.karslioglu@gmail.com>
2020-04-03 14:01:20 -0700

    Update

--- datum-intro/course.sh
+++ datum-intro/course.sh
@@ -1,2 +1,2 @@
-ssh root@[[HOST1_IP]] "minikube status"
+ssh root@[[HOST1_IP]] "curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb && sudo dpkg -i /tmp/pachctl.deb"
 
