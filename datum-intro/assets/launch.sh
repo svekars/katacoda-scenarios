cd9606bbdbcd52bae8179288ec613af0f8de8fcf
svekars <svetlana.karslioglu@gmail.com>
2020-04-03 13:57:49 -0700

    Update

--- datum-intro/assets/launch.sh
+++ datum-intro/assets/launch.sh
@@ -6,3 +6,7 @@ do
 done
 
 echo "Kubernetes Started"
+
+curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb && sudo dpkg -i /tmp/pachctl.deb
+
+pachctl deploy local
