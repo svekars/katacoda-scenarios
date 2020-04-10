b98225fdd124626a4719d25f5ba2e768cfe1c45a
svekars <svetlana.karslioglu@gmail.com>
2020-04-10 10:37:29 -0700

    Update

--- datum-intro/set-env.sh
+++ datum-intro/set-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Installing pachctl..."
 launch.sh
 
+echo "Installing pachctl..."
 curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb
 sudo dpkg -i /tmp/pachctl.deb
 chmod +x install-pachyderm.sh
