1c588a0d487ceae7023ba2ba449fe49fb8c735d9
svekars <svetlana.karslioglu@gmail.com>
2020-04-10 11:39:54 -0700

    Update

--- datum-intro/set-env.sh
+++ datum-intro/set-env.sh
@@ -2,18 +2,18 @@
 
 launch.sh
 
-echo "Installing pachctl..."
-curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb
-sudo dpkg -i /tmp/pachctl.deb
+#echo "Installing pachctl..."
+#curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb
+#sudo dpkg -i /tmp/pachctl.deb
 
-sleep 1
+# sleep 1
 
-chmod +x install-pachyderm.sh
-./install-pachyderm.sh
+# chmod +x install-pachyderm.sh
+#./install-pachyderm.sh
 
-while [ "$(kubectl get pod pachd -o jsonpath='{.status.containerStatuses[1].ready}')" != "true")]; do
-    sleep 5
-done
+#while [ "$(kubectl get pod pachd -o jsonpath='{.status.containerStatuses[1].ready}')" != "true")]; do
+#    sleep 5
+#done
 
-chmod +x deploy-pipeline.sh
-./deploy-pipeline.sh
+#chmod +x deploy-pipeline.sh
+# ./deploy-pipeline.sh
