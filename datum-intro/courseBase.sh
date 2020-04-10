37946689de00df9d7a4fbe24b4e600469a02b262
svekars <svetlana.karslioglu@gmail.com>
2020-04-10 11:28:34 -0700

    Update

--- datum-intro/courseBase.sh
+++ datum-intro/courseBase.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-launch.sh
+echo "Installing pachctl..."
+curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb
+sudo dpkg -i /tmp/pachctl.deb
 
-curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb && sudo dpkg -i /tmp/pachctl.deb
+sleep 1
+
 chmod +x install-pachyderm.sh
 ./install-pachyderm.sh
 
+while [ "$(kubectl get pod pachd -o jsonpath='{.status.containerStatuses[1].ready}')" != "true")]; do
+    sleep 5
+done
+
+chmod +x deploy-pipeline.sh
+./deploy-pipeline.sh
 
