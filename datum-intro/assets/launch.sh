15eb44aa5fe9959527b60a42b62bc3396e2780c8
svekars <svetlana.karslioglu@gmail.com>
2020-04-10 09:49:36 -0700

    Update

--- datum-intro/assets/launch.sh
+++ datum-intro/assets/launch.sh
@@ -1,5 +1,3 @@
-echo "Starting Kubernetes..."
-minikube start
 while [ `docker ps | wc -l` -eq 1 ]
 do
   sleep 1
--- datum-intro/set-env.sh
+++ datum-intro/set-env.sh
@@ -1,1 +1,7 @@
-launch.sh
+#!/bin/bash
+
+sleep 7;
+
+curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb && sudo dpkg -i /tmp/pachctl.deb
+chmod +x install-pachyderm.sh
+./install-pachyderm.sh
