0f3386a276e5679ba4b2c92b576fbeb12e42779b
svekars <svetlana.karslioglu@gmail.com>
2020-04-13 12:57:40 -0700

    Update

--- datum-intro/courseBase.sh
+++ datum-intro/courseBase.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sleep 10
-curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb
-sudo dpkg -i /tmp/pachctl.deb
+#sleep 10
+#curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_amd64.deb
+#sudo dpkg -i /tmp/pachctl.deb
 
 #sleep 1
 
