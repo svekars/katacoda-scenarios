6ef6452ab4fca1d7d73450e8f3cb77834f0e5d7b
svekars <svetlana.karslioglu@gmail.com>
2020-04-15 12:40:57 -0700

    Update

--- getting-started/step5-verify.sh
+++ getting-started/step5-verify.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#kubectl get pod -l app=pachd -o jsonpath="{.items[0].status.phase}"
-while [[ "$(kubectl get pod -l app=pachd | grep -v Running | grep -v Completed | wc -l)" != 1 ]]; do
-  sleep 1;
-done
+(($(kubectl get pod | grep pachd 2>/dev/null | wc -l) >= 1)) && echo "done"
