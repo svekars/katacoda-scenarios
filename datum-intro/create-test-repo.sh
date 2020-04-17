bb6841fdea6acc652521739ea62d6ec8092ad0bc
svekars <svetlana.karslioglu@gmail.com>
2020-04-17 15:11:05 -0700

    Update

--- datum-intro/create-test-repo.sh
+++ datum-intro/create-test-repo.sh
@@ -7,3 +7,8 @@ touch dir1/file{1..3}
 touch dir2/subdir1/file{1..3}
 touch dir2/subdir2/file{1..4}
 touch dir3/subdir1/file{1..3}
+
+pachctl create repo test
+pachctl put file -r test@master:/dir1 -f dir1/
+pachctl put file -r test@master:/dir2 -f dir2/
+pachctl put file -r test@master:/dir3 -f dir3/
